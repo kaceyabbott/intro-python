f5403a46fbb9843fadfd2af0bf9b4fc5de897960
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 13:02:36 -0600

    add if statement

--- Bytes.py
+++ Bytes.py
@@ -1,5 +1,34 @@
 """
 
 working with bytes
+bytes are similar to str type, but they are a sequence of
+bytes instead of unicode code points.
+
+use for raw binary data, fixed-widtd, single byte: ASCII
+
+use the byte() constructor
 """
+
+d = b'data'
+
+print(d, type(d))
+
+info = b'some bytes here'
+
+#split the bytes using split() method for bytes
+
+print(info.split())
+
+
+# encoding alr + 162 = ó
+
+message = "vamos al zoológico"
+
+print(message)
+
+data = message.encode("utf-8")
+print(data)
+
+message_decode = data.decode("utf-8")
+print(message_decode)
 
