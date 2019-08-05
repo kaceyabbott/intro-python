0f0a83ef3448e05af82a8e416ffb89ea1bd1b0f5
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 11:32:17 -0600

    add if statement

---
+++ strings.py
@@ -0,0 +1,33 @@
+"""
+Learning strings and collections
+
+A string is a immutable sequence of unicode code-points
+"""
+
+print('This is a string')
+print("this is a string too")
+
+
+# concatenation and adjacent strings
+
+s1 = "First"
+s2 = "Second"
+
+print(s1 + s2)
+
+# Multi line strings and new lines
+
+s3 = """This is a
+multi line
+string"""
+
+print(s3)
+
+s4 = "this is\na mulitline \nstring"
+print(s4)
+
+#Support for backslash
+
+s5 = "A\\in a string"
+
+print(s5)
