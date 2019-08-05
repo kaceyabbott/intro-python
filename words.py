5e434447daff635906941afc1f6381b837fb4862
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 16:04:55 -0600

    add if statement

--- words.py
+++ words.py
@@ -10,13 +10,15 @@ from urllib.request import urlopen
 
 
 
-def fetch_words():
+def fetch_words(file):
     """
+    count words in url file
+    :param filename: url to file
     fetch the words from a file on the web
     :return:
     """
-    file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
-    word_count = {}
+
+    word_count = []
     count = 0
     with urlopen(file) as story:
         for line in story:
@@ -24,26 +26,25 @@ def fetch_words():
             words = line.decode("utf-8").split() # split with space as seperator
             #print(words)
             for item in words:
-                #check if already in dictionary
-                if item in word_count:
-                    word_count[item] += 1
-                #if not then add to dictionary
-                else:
-                    word_count[item] = 1
-                count += 1
+                word_count.append(item)
 
+    return word_count
 
-    print("Total number of words", count)
-
-
-    #task 2 count number of times each work appears in file
-
-    print(word_count)
+def print_items(items):
+    """
+    print elements of the list
+    :param items: list
+    :return: nothing
+    """
+    for item in items:
+        print(item)
+    pass
 
 
 def main():
-    fetch_words()
-
+    file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
+    words = fetch_words(file)
+    print_items(words)
 
 if __name__ == '__main__':
     main()
