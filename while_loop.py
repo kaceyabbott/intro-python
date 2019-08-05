2e27d70b088a528e0140890690a5873146265124
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 11:17:50 -0600

    add if statement

--- while_loop.py
+++ while_loop.py
@@ -14,5 +14,21 @@ while counter != 0:
 
 print("outside while loop")
 
+counter = 5
+
+while counter:
+    print(counter)
+    counter -= 1
+
+print("outside while loop")
 
 
+# run forever
+while True:
+    print("enter a number")
+    response = input()  #take user input
+    if int(response) % 7 == 0:
+            break       #exit loop
+
+
+print("outside while loop")
