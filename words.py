0380583caf306e2104c5ea46c9e5e3e447e8ffc2
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 14:43:25 -0600

    add if statement

--- words.py
+++ words.py
@@ -5,7 +5,7 @@ task 1: count number of words in document
 """
 
 from urllib.request import urlopen
-
+word_count = {}
 file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
 count = 0
 with urlopen(file) as story:
@@ -14,7 +14,21 @@ with urlopen(file) as story:
         words = line.decode("utf-8").split() # split with space as seperator
         #print(words)
         for item in words:
+            #check if already in dictionary
+            if item in word_count:
+                word_count[item] += 1
+            #if not then add to dictionary
+            else:
+                word_count[item] = 1
             count += 1
 
 
 print("Total number of words", count)
+
+
+#task 2 count number of times each work appears in file
+
+print(word_count)
+
+
+
