67c01acab615d4a8a1f843b2633388991d81cb4a
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 15:45:47 -0600

    add if statement

--- words.py
+++ words.py
@@ -4,31 +4,48 @@ http://icarus.cs.weber.edu./~hvalle/hafb/words.txt
 task 1: count number of words in document
 """
 
+
 from urllib.request import urlopen
-word_count = {}
-file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
-count = 0
-with urlopen(file) as story:
-    for line in story:
-
-        words = line.decode("utf-8").split() # split with space as seperator
-        #print(words)
-        for item in words:
-            #check if already in dictionary
-            if item in word_count:
-                word_count[item] += 1
-            #if not then add to dictionary
-            else:
-                word_count[item] = 1
-            count += 1
 
 
-print("Total number of words", count)
 
 
-#task 2 count number of times each work appears in file
+def fetch_words():
+    """
+    fetch the words from a file on the web
+    :return:
+    """
+    file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
+    word_count = {}
+    count = 0
+    with urlopen(file) as story:
+        for line in story:
 
-print(word_count)
+            words = line.decode("utf-8").split() # split with space as seperator
+            #print(words)
+            for item in words:
+                #check if already in dictionary
+                if item in word_count:
+                    word_count[item] += 1
+                #if not then add to dictionary
+                else:
+                    word_count[item] = 1
+                count += 1
 
 
+    print("Total number of words", count)
+
+
+    #task 2 count number of times each work appears in file
+
+    print(word_count)
+
+
+def main():
+    fetch_words()
+
+
+if __name__ == '__main__':
+    main()
+    exit()
 
