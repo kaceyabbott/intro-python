b840d48daa425641c95d8fdb2dcda9f4c1618b6e
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 10:40:08 -0600

    intial commit

---
+++ intro-python.py
@@ -0,0 +1,46 @@
+# print ("hello world")
+
+# scalar data types: integers, floating point
+#None and boolean
+
+print("Binary notation", 0b0100)
+print("Hex notation", 0x0100)
+
+#use the int() constructor
+print(int("477"))
+
+# Option for the base
+print(int("1001", 2))
+
+#floating point: IEE-754
+# scientific notation
+
+print("scientific notatoin", 1.61e-35)
+print("Float contruction float()", float("1.99"))
+
+# Boolean
+
+print("Booliean const. bool(0)", bool(0))
+print("Booliean const. bool(42)", bool(42))
+print("Booliean const. bool(0.1)", bool(0.1))
+
+# Test boolean on collections
+print("Booliean const. bool([])", bool([]))
+print("Booliean const. bool([1,2,3])", bool([1,2,3]))
+print("Booliean const. bool('False')", bool("False"))
+
+
+# relational operators: ==, !=, <, >, <=, >=
+a = 12
+print("a == 12", a == 12)
+print("a != 12", a != 12)
+
+
+
+
+
+
+
+
+
+
