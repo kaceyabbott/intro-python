ce463c7b5f0ea156b4f2ca1518237b8c1e44410e
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 14:26:43 -0600

    add if statement

--- words.py
+++ words.py
@@ -1,4 +1,20 @@
 """
 getting file from web and counting words
-
+http://icarus.cs.weber.edu./~hvalle/hafb/words.txt
+task 1: count number of words in document
 """
+
+from urllib.request import urlopen
+
+file = "http://icarus.cs.weber.edu./~hvalle/hafb/words.txt"
+count = 0
+with urlopen(file) as story:
+    for line in story:
+
+        words = line.decode("utf-8").split() # split with space as seperator
+        #print(words)
+        for item in words:
+            count += 1
+
+
+print("Total number of words", count)
