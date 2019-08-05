ad0c9d10852c259528c8d5a83f27343eeaef1578
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 11:10:18 -0600

    add if statement

--- Control_Flow.py
+++ Control_Flow.py
@@ -8,4 +8,28 @@ Learn about control cflow in python.
 if True:
     print("it is true")
 
+if bool("eggs"):
+    print("yes please!")
+if "eggs":
+    print("Yes, why not")
+
+#flat is better than nested
+
+h = 101
+
+if h> 50:
+    print("greater than 50")
+    if h > 100:
+        print("greater than 100")
+
+elif h < 50 :  # else if
+
+    print('less than 50')
+
+else:
+    print("equals 50")
+
+
+
+#######
 print("done")
---
+++ while_loop.py
@@ -0,0 +1,16 @@
+"""
+
+Learn conditional repetition
+two loops: for loops and while loops
+
+"""
+
+counter = 5
+
+while counter != 0:
+    print(counter)
+    # augmented operators
+    counter -= 1
+
+
+
