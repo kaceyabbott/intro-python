8ce557017007992b2084b24943d0cc9d05cdd86a
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-05 13:35:23 -0600

    add if statement

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+"""
+working with dictionaries (hashes)
+dictionaries maps keys to values.
+in some languages are know as associative arrays, hashes, or maps
+create them using {} containing a key value pair
+retrieve them by [key value]
+"""
+
+d = {'alice':'801-123-45-8998',
+     'pedro': '956-445-78-8996',
+     'john':'654-321-66-4477'}
+
+print(d, type(d))
+
+print(d['alice'])
+
+
+
