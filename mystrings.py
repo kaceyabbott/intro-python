342471c692d7d9e8f8b71eca2a665c9e41c34d47
kaceyabbott <kacey.abbott@us.af.mil>
2019-08-06 15:58:03 -0600

    Time and youtube video

--- mystrings.py
+++ mystrings.py
@@ -19,6 +19,26 @@ def main():
     teams = ["real madrid", "barcelona", "manchester united"]
     combine = " : ".join(teams)
     print(combine)
+    #split record
+    print(combine.split(" : "))
+    departure, _, arrival = "london:edinburgh".partition(":")
+    print(departure, arrival)
+
+    print("the age of {0} is {1}".format("mario", 34))
+    print("the age of {0} is {1}, and the birthday of {0} is {2}".format("mario", 34, "august 12"))
+
+    print("the age of {} is {}".format("mario", 34))
+
+    #by field name
+    print("current position {latitude} {longitude}".format(latitude = "60 N", longitude = "5E"))
+
+    x = 10
+    y = 20
+    #prints elements of list
+    print("galatic position x = {pos[0]}, y = {pos[1]}".format(pos=(x,y)))
+
+
+
 
 
 if __name__ == '__main__':
